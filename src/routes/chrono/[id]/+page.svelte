63f71bb1bdb6e1d74d48acbe1714b065822df048
Jordi <jordi@Jordis-Mac-mini.home>
2023-07-08 11:30:51 +0200

    color transition

--- src/routes/chrono/[id]/+page.svelte
+++ src/routes/chrono/[id]/+page.svelte
@@ -1,44 +1,49 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
+	import { onMount, tick } from 'svelte';
 	import { getSequentialTimeNodeFromChronoNode } from '$lib/utils';
 	import chronoStore, { type TimeNode } from '$lib/store';
+	import { browser } from '$app/environment';
 	import headerStore from '$lib/headerStore';
 
 	headerStore.set({ size: 'sm', route: 'chrono', search: '', selected: [] });
 
-	onMount(() => {
-		const chrono = $chronoStore.find((chrono) => chrono.id === $page.params.id);
+	$: browser && init($page.params.id)
+
+    async function init(id: string) {
+        const chrono = $chronoStore.find((chrono) => chrono.id === id);
 		if (!chrono) {
-			goto('/');
-		} else {
-			nodes = getSequentialTimeNodeFromChronoNode(chrono.nodes);
+			return goto('/');
 		}
-	});
+        await tick()
+        nodes = getSequentialTimeNodeFromChronoNode(chrono.nodes);
+    }
 
 	let nodes: TimeNode[] = [];
 	let currentIndex = 0;
 	let currentTime = 0;
+    let currentNode;
 	$: currentNode = nodes[currentIndex];
-
-	$: console.log(nodes);
 
 	function handleOnClick() {
 		console.log('handleOnClick');
 	}
 </script>
 
-{#if currentNode}
-	<button on:click={handleOnClick} class="w-full h-full bg-red-600">
-		<div class="text-5xl text-center">
-			<h4>{currentNode.title}</h4>
-			{#if currentNode.parentTitle}
-				<h5 class="text-3xl">{currentNode.parentTitle}</h5>
-			{/if}
-		</div>
-		<div class="text-6xl text-center top-[40%]">
-			{currentNode.duration}
-		</div>
-	</button>
-{/if}
+<div class="w-full h-full transition-all duration-700" style:background-color={currentNode?.color ?? 'bg-slate-800'}>
+
+    {#if currentNode}
+        <button on:click={handleOnClick} class="w-full h-full">
+            <div class="text-5xl text-center">
+                <h4>{currentNode.title}</h4>
+                {#if currentNode.parentTitle}
+                    <h5 class="text-3xl">{currentNode.parentTitle}</h5>
+                {/if}
+            </div>
+            <div class="text-6xl text-center top-[40%]">
+                {currentNode.duration}
+            </div>
+        </button>
+    {/if}
+</div>
